410da5b92eba3ede3904ecfd9377e022fd04a42b
Stephen Richer <sr467@bath.ac.uk>
2020-04-22 15:21:14 +0100

    Add ability to generate config with links.

--- workflow/scripts/generate_config.py
+++ workflow/scripts/generate_config.py
@@ -24,37 +24,39 @@ def main():
     parser.add_argument(
         '-t', '--tads', nargs = '*', default = None,
         help = 'TAD scores in ".links" format.')
-
     parser.add_argument(
         '-l', '--loops', nargs = '*', default = None,
         help = 'Loop output.')
-
     parser.add_argument(
         '-m', '--matrix',
         help = 'HiC matrix.')
-
     parser.add_argument(
         '-s', '--sample', default = 'sample',
         help = 'Sample name')
-
+    parser.add_argument(
+        '--links', nargs=2, default=None,
+        help = 'UP and DOWN links files showing differential interactions.')
     parser.add_argument(
         '-c', '--ctcfs', nargs = '+', default = None,
         help = 'CTCF position in bigWig format.')
-
     parser.add_argument(
         '-r', '--ctcf_orientation',
-        help = 'CTCF orientations in BED format.'
-    )
-
+        help = 'CTCF orientations in BED format.')
     parser.add_argument(
         '-g', '--genes',
-        help = 'Genes in BED format.'
-    )
-
+        help = 'Genes in BED format.')
     parser.add_argument(
         '-d', '--depth', type = int, default = 1000000,
-        help = 'HiC matrix depth.'
-    )
+        help = 'HiC matrix depth.')
+    parser.add_argument(
+        '--colourmap', default='Purples',
+        help = 'Matplotlib coluor map to use for the heatmap.')
+    parser.add_argument(
+        '--vmin', type = int, default = 0,
+        help = 'Minimum score value.')
+    parser.add_argument(
+        '--vmax', type = int, default = 2,
+        help = 'Minimum score value.')
 
     args = parser.parse_args()
     func = args.function
@@ -64,12 +66,16 @@ def main():
     return func(**vars(args))
 
 
-def make_config(insulations, sample, matrix, tads, loops,
-                ctcfs, ctcf_orientation, genes, depth):
+def make_config(insulations, sample, matrix, tads, loops, links, ctcfs,
+                ctcf_orientation, genes, depth, colourmap, vmin, vmax):
+
+    # Detect use of HiC comparision and replace dash with newline to fit
+    if '-over-' in sample:
+        sample.replace('-', '\n')
 
     print('[spacer]')
     if matrix and not_empty(matrix):
-        write_matrix(sample, matrix, depth)
+        write_matrix(sample, matrix, colourmap, depth, vmin, vmax)
 
     if loops is not None:
         for i, loop in enumerate(loops):
@@ -88,6 +94,20 @@ def make_config(insulations, sample, matrix, tads, loops,
             if not_empty(insulation):
                 write_insulation(sample = sample , insulation = insulation, i = i)
         print('[spacer]')
+
+
+    if links is not None:
+        for i, link in enumerate(links):
+            if i == 0:
+                overlay = False
+                direction = 'up'
+            else:
+                overlay = True
+                direction = 'down'
+            if not_empty(link):
+                write_links(link, overlay=overlay, direction=direction)
+        print('[spacer]')
+
 
     print('# End Sample Specific')
     if ctcfs is not None:
@@ -108,16 +128,18 @@ def make_config(insulations, sample, matrix, tads, loops,
 def not_empty(path):
     return os.path.exists(path) and os.path.getsize(path) > 0
 
-def write_matrix(sample, matrix, depth = 1000000):
+def write_matrix(
+        sample, matrix, colourmap='Purples', depth=1000000, vmin=0, vmax=2):
     print(f'[{sample} - matrix]',
           f'file = {matrix}',
           f'title = {sample}',
           f'depth = {depth}',
-          f'min_value = 0',
-          f'max_value = 2',
-          f'colormap = Purples',
+          f'min_value = {vmin}',
+          f'max_value = {vmax}',
+          f'colormap = {colourmap}',
           f'show_masked_bins = true',
           f'file_type = hic_matrix', sep = '\n')
+
 
 def write_loops(sample, loops, i):
 
@@ -128,6 +150,7 @@ def write_loops(sample, loops, i):
           f'overlay_previous = share-y',
           f'file_type = links',
           f'line_width = 5', sep = '\n')
+
 
 def write_tads(sample, tads, i,
         colours = ['#1b9e7780', '#d95f0280', '#d902d980', '#00000080'],
@@ -143,6 +166,7 @@ def write_tads(sample, tads, i,
           f'overlay_previous = share-y',
           f'line_width = 1.5', sep = '\n')
 
+
 def write_insulation(sample, insulation, i,
         colours = ['#1b9e7780', '#d95f0280', '#d902d980', '#00000080']):
     colour = colours[i]
@@ -156,6 +180,33 @@ def write_insulation(sample, insulation, i,
           f'file_type = bedgraph_matrix',
           f'type = lines',
           f'overlay_previous = {overlay}', sep = '\n')
+
+
+def write_links(link, direction, overlay=False):
+
+    overlay = 'share-y' if overlay else 'no'
+    colour = 'Reds' if direction == 'up' else 'Blues'
+    group1, group2 = get_links_groups(link)
+    if direction == 'up':
+        title = f'Differential interactions - Red (UP in {group2})'
+    else:
+        title = ''
+
+    print(f'[{group1} vs {group2} - DI {direction}]',
+          f'file = {link}',
+          f'title = {title}',
+          f'line_style = dashed',
+          f'color = {colour}',
+          f'height = 10',
+          f'overlay_previous = {overlay}',
+          f'file_type = links', sep = '\n')
+
+
+def get_links_groups(path):
+    """ Retrieve group name pairs from links path. """
+    base = os.path.basename(path).split('-')
+    return base[0], base[2]
+
 
 def write_ctcf(ctcf, i,
     colours = ['#FF000080', '#0000FF80', '#00FF0080' ]):
