a855a4ab443ad8662206389f8a5995fc35894961
Stephen Richer <sr467@bath.ac.uk>
2020-04-30 22:18:26 +0100

    Fix incorporate coordinate scaling.

--- workflow/scripts/reformat_ontad.sh
+++ workflow/scripts/reformat_ontad.sh
@@ -2,12 +2,15 @@
 
 main() {
   local start="${1}"
-  local input="${2}"
+  local bin="${2}"
+  local input="${3}"
 
+  # Ontad BED coordinates are the END of the bin region
+  # Therefore we rescale the intervals to start = n - bin, end = n
   tail -n+2 "${input}" \
-      | awk -v start="${start}" -v OFS='\t' '
-          {$2=$2+start; $3=$3+start; $7=$7+start; $8=$8+start}
-          {print $1, $2, $3, $1, $7, $8, 0}'
+      | awk -v start="${start}" -v bin="${bin}" -v OFS='\t' '
+          {$2=$2+start; $3=$3+start}
+          {print $1, $2-bin, $2, $1, $3-bin, $3, 0}'
 
 }
 
