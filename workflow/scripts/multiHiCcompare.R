ef9863e5ee032a05c4d38b0a3e41b254f8794dd7
Stephen Richer <sr467@bath.ac.uk>
2020-10-01 15:55:46 +0100

    Add QC directory for saving QC plots seperately and fix bug when non-numeric chromosome names are provided.

--- workflow/scripts/multiHiCcompare.R
+++ workflow/scripts/multiHiCcompare.R
@@ -2,7 +2,7 @@
 
 library(reshape2)
 library(multiHiCcompare)
-
+library(GenomicRanges)
 # Prevent scientific notation
 options(scipen=999)
 
@@ -45,14 +45,15 @@ writeMatrix <- function(hic.table, out, chr, start, end, binsize, var) {
 args = commandArgs(trailingOnly=TRUE)
 
 outdir = args[1]
-chr = args[2]
-start = as.integer(args[3])
-end = as.integer(args[4])
-binsize = as.integer(args[5])
-fdr = as.double(args[6])
-matrices = args[-c(1:6)]
+qcdir = args[2]
+chr = args[3]
+start = as.integer(args[4])
+end = as.integer(args[5])
+binsize = as.integer(args[6])
+fdr = as.double(args[7])
+matrices = args[-c(1:7)]
 
-data(hg38_cyto)
+dir.create(qcdir)
 
 args = vector("list", length(matrices))
 groups = c()
@@ -60,21 +61,17 @@ n = 1
 for (matrix in matrices) {
   name = split_name(matrix)
   groups = c(groups, name$group)
-  data = cbind(chr, read.table(matrix))
+  # Substitute chr name with 99 as multiHiCcompare requires numeric.
+  data = cbind(99, read.table(matrix))
   args[[n]] = data
   n = n + 1
 }
-args[['groups']] = groups
-args[['zero.p']] = 0.8
-args[['A.min']] = 5
-args[['filter']] = TRUE
-args[['remove.regions']] = hg38_cyto
 
-hicexp = do.call('make_hicexp', args)
-
+hicexp = make_hicexp(data_list = args, groups = groups, zero.p = 0.8, 
+                     A.min = 5, filter = TRUE, remove.regions = NULL)
 hicexp <- cyclic_loess(hicexp, verbose = FALSE, parallel = FALSE, span = 0.2)
 
-md_plot = paste(outdir, '/', unique(groups)[1], '-vs-', unique(groups)[2], '-loess.png', sep = '')
+md_plot = paste(qcdir, '/', unique(groups)[1], '-vs-', unique(groups)[2], '-loess.png', sep = '')
 png(md_plot)
 MD_hicexp(hicexp)
 dev.off()
