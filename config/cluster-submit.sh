837c7995f8c6767983df0214d978e5f0bd91f95b
Stephen Richer <sr467@bath.ac.uk>
2020-10-01 22:32:38 +0100

    Remove unused.

--- config/cluster-submit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-config=/home/u/sr467/scratch/projects/HiC-new-pipeline/config/config.yaml
-snakemake --profile slurm --use-conda -kp --notemp --configfile "${config}" \
---conda-prefix /home/u/sr467/scratch/projects/HiC-pipeline/.snakemake \
---cluster-config /home/u/sr467/scratch/projects/HiC-new-pipeline/config/cluster-config.yaml \
---wrapper-prefix file:/home/u/sr467/scratch/snakemake-wrappers/ "${@}"
--- config/local-submit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-config=/media/stephen/Data/HiC-subsample/config/local-config.yaml
-snakemake --use-conda -kp --notemp --configfile "${config}" \
---conda-prefix /media/stephen/Data/HiC-subsample/analysis/.snakemake "${@}"
