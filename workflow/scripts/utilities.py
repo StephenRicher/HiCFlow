62c3721573c227f8ad53f72e57e6b563619c4e19
Stephen Richer <sr467@bath.ac.uk>
2020-12-09 16:48:04 +0000

    Add readHomer.

--- workflow/scripts/utilities.py
+++ workflow/scripts/utilities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import pandas as pd
 
 
 def setDefaults(parser, verbose=True, version=None):
@@ -22,3 +23,24 @@ def setDefaults(parser, verbose=True, version=None):
         args = parser.parse_args()
 
     return args
+
+
+def readHomer(matrix, binSize):
+    """ Read Homer matrix format and convert to long format """
+
+    mat = pd.read_csv(matrix, skiprows=1, header=None, sep='\t').drop(0, axis=1)
+    # Split chromosome and start into 2 column DF
+    positions = mat[1].str.split('-', expand=True)
+    # Add end positions
+    positions[2] = positions[1].astype(int) + binSize
+    # Add full region position and set as index
+    positions['pos'] = mat[1]
+    positions =  positions.set_index('pos')
+    # Set columns
+    mat.columns = pd.Series('region').append(positions[1])
+    mat['end'] = positions[1].values.astype(int)
+
+    mat = mat.melt(id_vars=['region', 'end'], var_name='start', value_name='score')
+    mat['start'] = mat['start'].astype(int)
+
+    return positions, mat
