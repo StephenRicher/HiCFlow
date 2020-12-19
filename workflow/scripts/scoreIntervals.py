da6b89e311d7d3c92ba2ba8da85e6906707a5b02
Stephen Richer <sr467@bath.ac.uk>
2020-12-19 11:02:23 +0000

    Major update to increase speed by orders of magnitude.

--- workflow/scripts/scoreIntervals.py
+++ workflow/scripts/scoreIntervals.py
@@ -12,22 +12,56 @@ from bedgraphUtils import splitScore, splitPos, splitName
 __version__ = '1.0.0'
 
 
-def scoreIntervals(bedGraph: str, bed: str, name: str, format: str):
+def scoreIntervals(bedGraph: str, bed: str):
+    bedgraph = readBedgraph(bedGraph)
     regions = readBed(bed)
     scoredRegions = defaultdict(float)
-    with open(bedGraph) as fh:
-        for line in fh:
-            chrom, start, end, score = splitScore(line, format)
-            for name, interval in regions[chrom]:
+    for chrom, intervals in regions.items():
+        for name, interval in intervals:
+            key = f'{chrom} {min(interval)} {max(interval)+1} {name}'
+            validRanges = getValidRanges(interval, list(bedgraph[chrom].keys()))
+            if not validRanges:
+                continue
+            for validRange in validRanges:
                 # Detect base overlap between bedgraph interval and each region
-                overlap = range(max(start, min(interval)), min(end, max(interval))+1)
-                if overlap:
-                    regionLength = end - start
-                    key = f'{chrom} {min(interval)} {max(interval)+1} {name}'
-                    scoredRegions[key] += (score / regionLength) * len(overlap)
-    for region, score in scoredRegions.items():
-        chrom, start, end, name = region.split()
-        print(chrom, start, end, name, score, sep='\t')
+                overlap = getOverlap(validRange, interval)
+                score = bedgraph[chrom][validRange] * len(overlap)
+                scoredRegions[key] += score
+            print(chrom, min(interval), max(interval)+1,
+                  name, scoredRegions[key], sep='\t')
+
+
+def getOverlap(range1, range2):
+    return range(max(min(range1), min(range2)), min(max(range1), max(range2))+1)
+
+
+def getValidRanges(interval, rangeList):
+    """ Return range objects that overlap the interval.
+        Must provided sorted list of ranges. """
+
+    ranges = []
+    minInterval = min(interval)
+    maxInterval = max(interval)
+    for i, rangeObj in enumerate(rangeList):
+        if minInterval in rangeObj or maxInterval in rangeObj:
+            ranges.append(rangeObj)
+        if min(rangeObj) > maxInterval:
+            break
+    return ranges
+
+
+def readBedgraph(file):
+    """ Construct bedgraph dictionary per chromosome with interval ranges """
+    bedgraph = defaultdict(dict)
+    with open(file) as fh:
+        for line in fh:
+            chrom, start, end, score = splitScore(line, filetype='bedgraph')
+            regionLength = end - start
+            bedgraph[chrom][range(start, end)] = score / regionLength
+    # Sort per-chromosome ranges by start
+    for chrom, intervals in bedgraph.items():
+        bedgraph[chrom] = dict(sorted(intervals.items(), key=lambda r: r[0].start))
+    return bedgraph
 
 
 def readBed(bed):
@@ -58,12 +92,6 @@ def parseArgs():
         'bedGraph', help='BedGraph/BED interval file to rescale.')
     parser.add_argument(
         'bed', metavar='BED', help='BED file indicating regions to process.')
-    parser.add_argument(
-        '--name', help='Name to store in metadata. Defaults to infile path.')
-    requiredNamed = parser.add_argument_group('required named arguments')
-    requiredNamed.add_argument(
-        '--format',  required=True, choices=['bed', 'bedgraph'],
-        help='Input format to correctly retrive score column.')
     parser.set_defaults(function=scoreIntervals)
 
     return setDefaults(parser)
