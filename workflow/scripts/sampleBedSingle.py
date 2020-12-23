0a876f128cea83c8c392afa8c1b9077f17ca45b2
Stephen Richer <sr467@bath.ac.uk>
2020-12-23 22:57:31 +0000

    Update to allow multiple reps and multiple intervals.

--- workflow/scripts/sampleBedSingle.py
+++ workflow/scripts/sampleBedSingle.py
@@ -14,16 +14,18 @@ from utilities import setDefaults, createMainParent
 __version__ = '1.0.0'
 
 
-def sampleIntervals(bed: str, nRepeats: int, length: int, seed: float):
+def sampleIntervals(bed: str, name: str, nRepeats: int, nIntervals: int, length: int, seed: float):
     random.seed(seed)
     allEntries = readBed(bed)
-    # Select BEDS, weight by length, with replacement
-    selections = random.choices(
-        list(allEntries.keys()),
-        weights=list(allEntries.values()), k=nRepeats)
-    for selection in selections:
-        pos = random.choice(selection.interval)
-        print(selection.chrom, pos, pos + length, sep='\t')
+    name = bed if name is None else name
+    for repeat in range(nRepeats):
+        # Select BEDS, weight by length, with replacement
+        selections = random.choices(
+            list(allEntries.keys()),
+            weights=list(allEntries.values()), k=nIntervals)
+        for selection in selections:
+            pos = random.choice(selection.interval)
+            print(selection.chrom, pos, pos + length, f'{name}-{repeat}', sep='\t')
 
 
 def readBed(file):
@@ -49,11 +51,17 @@ def parseArgs():
         'bed', metavar='BED',
         help='BED intervals within which regions will be sampled .')
     parser.add_argument(
+        '--name',
+        help='Name to append to BED entry - default to BED filename')
+    parser.add_argument(
         '--length', default=1, type=int,
         help='The length of the intervals to generate.')
     parser.add_argument(
-        '--nRepeats', default=100_000, type=int,
-        help='Number of intervals to generate samples (default: %(default)s).')
+        '--nIntervals', default=10_000, type=int,
+        help='Number number of intervals per repeat (default: %(default)s).')
+    parser.add_argument(
+        '--nRepeats', default=1000, type=int,
+        help='Number of repeats (default: %(default)s).')
     parser.add_argument(
         '--seed', default=None, type=float,
         help='Seed for random number generation (default: %(default)s)')
