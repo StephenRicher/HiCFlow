572a6744522cf16c054fc603182d6ec154a10a15
Stephen Richer <sr467@bath.ac.uk>
2021-01-08 10:18:04 +0000

    Update readHomer utiliity function to more general purpose.

--- workflow/scripts/utilities.py
+++ workflow/scripts/utilities.py
@@ -44,19 +44,30 @@ def createMainParent(verbose=True, version=None):
 def readHomer(matrix, binSize):
     """ Read Homer matrix format and convert to long format """
 
+    # Read Homer as pandas
     mat = pd.read_csv(matrix, skiprows=1, header=None, sep='\t').drop(0, axis=1)
+
     # Split chromosome and start into 2 column DF
     positions = mat[1].str.split('-', expand=True)
+    positions[1] = positions[1].astype(int)
     # Add end positions
-    positions[2] = positions[1].astype(int) + binSize
+    positions[2] = positions[1] + binSize
     # Add full region position and set as index
     positions['pos'] = mat[1]
     positions =  positions.set_index('pos')
-    # Set columns
-    mat.columns = pd.Series('region').append(positions[1])
-    mat['end'] = positions[1].values.astype(int)
 
-    mat = mat.melt(id_vars=['region', 'end'], var_name='start', value_name='score')
-    mat['start'] = mat['start'].astype(int)
+    # Drop region column
+    mat = mat.drop(1, axis=1)
+    # Set index and columns as start positions
+    mat.columns = list(positions[1])
+
+    mat['start'] = mat.columns
+    mat = mat.melt(id_vars=['start'], var_name='start2', value_name='score')
+    mat['end'] = mat['start'] + binSize
+    mat['end2'] = mat['start2'] + binSize
+
+    # Extract chrom - assume matrix must be cis
+    chrom = f'{positions[0][0]}-'
+    mat['region'] = chrom + mat['start'].astype(str)
 
     return positions, mat
