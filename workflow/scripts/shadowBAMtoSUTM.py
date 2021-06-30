1b0c34a8e9e65ec6cedd68e93d49d0f62ba46fe5
Stephen Richer <sr467@bath.ac.uk>
2021-06-30 21:53:05 +0100

    Allow multiple shadow selection

--- workflow/scripts/shadowBAMtoSUTM.py
+++ workflow/scripts/shadowBAMtoSUTM.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-""" Radomly split SAM files and write to SUTM """
+""" Perform permutation testing by merging BAMs from both groups and randomly
+    split (keeping the relative sample sizes approximately equal). Output is
+    written to SUTM format and is repeated a determined number of times. """
 
 import os
 import sys
@@ -10,17 +12,22 @@ import argparse
 import pandas as pd
 from typing import List
 from collections import defaultdict
-from utilities import setDefaults, createMainParent
+from utilities import setDefaults, createMainParent, getGroup, getBin
 
 __version__ = '1.0.0'
 
 
-def homer2sutm(BAMs: List, binSize: int, out1: str, out2: str, bamLogs: List, seed: int):
+def homer2sutm(
+        BAMs: List, binSize: int, start: int, prefix1: str, prefix2: str,
+        bamLogs: List, nShadow: int, seed: int):
 
+
+    assert len(set([getGroup(bam) for bam in BAMs])) == 2
     random.seed(seed)
 
-    split1 = defaultdict(lambda: defaultdict(int))
-    split2 = defaultdict(lambda: defaultdict(int))
+    # Create array to store shadowed counts
+    split1 = [defaultdict(lambda: defaultdict(int)) for i in range(nShadow)]
+    split2 = [defaultdict(lambda: defaultdict(int)) for i in range(nShadow)]
 
     if bamLogs is not None:
         group, probSplit = readBAMLogs(bamLogs)
@@ -28,53 +35,49 @@ def homer2sutm(BAMs: List, binSize: int, out1: str, out2: str, bamLogs: List, se
         probSplit = 0.5
 
     for bam in BAMs:
+        group = getGroup(bam)
         samfile = pysam.AlignmentFile(bam, 'rb')
-        for i, record in enumerate(samfile.fetch(until_eof=True)):
-            if i % 2 == 0:
+        for n, record in enumerate(samfile.fetch(until_eof=True)):
+            if n % 2 == 0:
                 continue
             positions = sorted([
-                getBin(record.next_reference_start, binSize),
-                getBin(record.reference_start, binSize)])
-            if random.random() > probSplit:
-                split1[positions[0]][positions[1]] += 1
-            else:
-                split2[positions[0]][positions[1]] += 1
-
-    split1 = [(k, k1, v1) for k, v in split1.items() for k1, v1 in v.items()]
-    split2 = [(k, k1, v1) for k, v in split2.items() for k1, v1 in v.items()]
+                getBin(record.next_reference_start, binSize, start),
+                getBin(record.reference_start, binSize, start)])
+            for i in range(nShadow):
+                if random.random() > probSplit:
+                    split1[i][positions[0]][positions[1]] += 1
+                else:
+                    split2[i][positions[0]][positions[1]] += 1
 
     # Ensure the correct split is written to the corect group
-    if bamLogs and (group == getGroup(out1)):
-        split1Out = out2
-        split2Out = out1
+    if bamLogs and (group == getGroup(prefix1)):
+        split1Out = prefix2
+        split2Out = prefix1
     else:
-        split1Out = out1
-        split2Out = out2
+        split1Out = prefix1
+        split2Out = prefix2
 
-    pd.DataFrame(split1).sort_values([0, 1]).to_csv(split1Out, header=False, index=False)
-    pd.DataFrame(split2).sort_values([0, 1]).to_csv(split2Out, header=False, index=False)
+    for i in range(nShadow):
+        shadowSplit1 = [(k, k1, v1) for k, v in split1[i].items() for k1, v1 in v.items()]
+        shadowSplit2 = [(k, k1, v1) for k, v in split2[i].items() for k1, v1 in v.items()]
+
+        pd.DataFrame(shadowSplit1).sort_values([0, 1]).to_csv(
+            f'{prefix1}-{i}-sutm.txt', sep=' ', header=False, index=False)
+        pd.DataFrame(shadowSplit2).sort_values([0, 1]).to_csv(
+            f'{prefix2}-{i}-sutm.txt', sep=' ', header=False, index=False)
 
 
 def readBAMLogs(logs):
-    """ Read bam logs to determine group split ratio """
+    """ Read bam logs and cout Hi-C contacts to
+        determine group split ratio. """
     count = defaultdict(int)
     for log in logs:
         log = pd.read_csv(log, sep='\t')
-        group = getGroup(log['File'])
+        group = getGroup(str(log['File']))
         count[group] += int(log['Hi-C contacts'])
     assert len(count) == 2
     # Only need to return 1 groups information
-    return group, count[group] / sum(count.values)
-
-
-def getGroup(path):
-    """ Retrieve group name from file path """
-    basename = os.path.basename(path)
-    return basename.split('-')[0]
-
-
-def getBin(pos: int, binSize: int):
-    return pos - (pos % binSize)
+    return group, count[group] / sum(count.values())
 
 
 def parseArgs():
@@ -86,6 +89,9 @@ def parseArgs():
     parser.add_argument(
         'BAMs', nargs='*', help='BAM format alignment files.')
     parser.add_argument(
+        '--nShadow', default=1, type=int,
+        help='Number of shadow matrices to generate (default: %(default)s)')
+    parser.add_argument(
         '--seed', default=None, type=int,
         help='Seed for random swap (default: %(default)s)')
     parser.add_argument(
@@ -94,11 +100,17 @@ def parseArgs():
              'keep size ratio of groups equal.')
     requiredNamed = parser.add_argument_group('required named arguments')
     requiredNamed.add_argument(
-        '--binSize', type=int, required=True, help='Bin size of matrix.')
+        '--binSize', type=int, required=True,
+        help='Bin size of matrix.')
     requiredNamed.add_argument(
-        '--out1', required=True, help='Output of matrix 1 in SUTM format.')
+        '--start', type=int, required=True,
+        help='Start position (0-based) of matrix.')
     requiredNamed.add_argument(
-        '--out2', required=True, help='Output of matrix 2 in SUTM format.')
+        '--prefix1', required=True,
+        help='Path prefix of shadow matrices (group 1) in SUTM format.')
+    requiredNamed.add_argument(
+        '--prefix2', required=True,
+        help='Path prefix of shadow matrices (group 2) in SUTM format.')
     parser.set_defaults(function=homer2sutm)
 
     return setDefaults(parser)
