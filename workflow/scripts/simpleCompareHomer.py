288922eabad257ef94396142619d424afed77d0c
Stephen Richer <sr467@bath.ac.uk>
2021-07-07 12:36:37 +0100

    Modify method

--- workflow/scripts/simpleCompareHomer.py
+++ workflow/scripts/simpleCompareHomer.py
@@ -23,20 +23,10 @@ def simpleSubtract(matrices: List, outFileName: str):
         sys.exit("The two matrices have different size. Use matrices having "
                  "the same resolution and created using the same parameters. "
                  "Check the matrix values using the tool `hicInfo`.")
-    # normalize by total matrix sum
-    hic1.matrix.data =  hic1.matrix.data.astype(float) / hic1.matrix.data.sum()
-    hic2.matrix.data =  hic2.matrix.data.astype(float) / hic2.matrix.data.sum()
 
     nan_bins = set(hic1.nan_bins)
     nan_bins = nan_bins.union(hic2.nan_bins)
-
-    hic2.matrix.data = float(1) / hic2.matrix.data
-    new_matrix = hic1.matrix.multiply(hic2.matrix)
-    # just in case
-    new_matrix.eliminate_zeros()
-    new_matrix.data = np.log2(new_matrix.data)
-    new_matrix.eliminate_zeros()
-
+    new_matrix = hic2.matrix - hic1.matrix
     hic1.setMatrixValues(new_matrix)
     hic1.maskBins(sorted(nan_bins))
     hic1.save(outFileName)
