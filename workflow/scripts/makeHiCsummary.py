02c67bc3ccdde88b87c2a8667bd1f68ed9a5be17
Stephen Richer <sr467@bath.ac.uk>
2021-11-19 22:52:44 +0000

    Simplify.

--- workflow/scripts/makeHiCsummary.py
+++ workflow/scripts/makeHiCsummary.py
@@ -11,31 +11,35 @@ from utilities import setDefaults, createMainParent
 __version__ = '1.0.0'
 
 
-def makeHiCsummary(matrix: str, chrom: str, start: int, binSize: int):
-    # Use path to set seed for random number generator
-    random.seed(matrix)
-    matrix = hm.hiCMatrix(matrix)
-    # Ensure chrom name is prefixed with chr
-    chrom = f'chr{chrom}' if not chrom.startswith('chr') else chrom
-    # Get indices of non-zero elements
-    rows, columns = matrix.matrix.nonzero()
-    for r, c in zip(rows, columns):
-        # Don't count lower half of matrix
-        if r > c:
-            continue
-        # Write entries according to number of contacts at that position
-        for val in range(matrix.matrix[r, c]):
-            startBase1 = start + (r * binSize)
-            baseRange1 = range(startBase1, startBase1 + binSize)
+def makeHiCsummary(matrices):
+    for matrix in matrices:
+        # Use path to set seed for random number generator
+        random.seed(matrix)
+        matrix = hm.hiCMatrix(matrix)
+        binSize = matrix.getBinSize()
+        start = matrix.getBinPos(0)[1] # 1-based start position
+        # Ensure chrom name is prefixed with chr
+        chrom = str(matrix.getChrNames()[0])
+        chrom = f'chr{chrom}' if not chrom.startswith('chr') else chrom
+        # Get indices of non-zero elements
+        rows, columns = matrix.matrix.nonzero()
+        for r, c in zip(rows, columns):
+            # Don't count lower half of matrix
+            if r > c:
+                continue
+            # Write entries according to number of contacts at that position
+            for val in range(int(matrix.matrix[r, c])):
+                startBase1 = start + (r * binSize)
+                baseRange1 = range(startBase1, startBase1 + binSize)
 
-            startBase2 = start + (c * binSize)
-            baseRange2 = range(startBase2, startBase2 + binSize)
+                startBase2 = start + (c * binSize)
+                baseRange2 = range(startBase2, startBase2 + binSize)
 
-            # Generate pseudo-random position within bin range
-            pos1 = random.choice(baseRange1)
-            pos2 = random.choice(baseRange2)
+                # Generate pseudo-random position within bin range
+                pos1 = random.choice(baseRange1)
+                pos2 = random.choice(baseRange2)
 
-            print(',', chrom, pos1, '+', chrom, pos2, '+', sep='\t')
+                print(',', chrom, pos1, '+', chrom, pos2, '+', sep='\t')
 
 
 def parseArgs():
@@ -44,15 +48,7 @@ def parseArgs():
     mainParent = createMainParent(verbose=False, version=__version__)
     parser = argparse.ArgumentParser(
         epilog=epilog, description=__doc__, parents=[mainParent])
-    parser.add_argument(
-        'matrix', help='Matrix in homer format.')
-    requiredNamed = parser.add_argument_group('required named arguments')
-    requiredNamed.add_argument(
-        '--chrom', required=True, help='Chromosome of matrix.')
-    requiredNamed.add_argument(
-        '--start', type=int, required=True, help='Start position of matrix.')
-    requiredNamed.add_argument(
-        '--binSize', type=int, required=True, help='Bin size of matrix.')
+    parser.add_argument('matrices', nargs='+', help='Matrices in h5 format.')
     parser.set_defaults(function=makeHiCsummary)
 
     return setDefaults(parser)
