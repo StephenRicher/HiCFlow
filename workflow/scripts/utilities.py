920a9744083f1273701e11d3aa0948a241c30c9b
Stephen Richer <sr467@bath.ac.uk>
2022-01-06 10:01:40 +0000

    Removed unused.

--- workflow/scripts/utilities.py
+++ workflow/scripts/utilities.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 import logging
 import argparse
-import itertools
-import contextlib
-import numpy as np
-import pandas as pd
-from collections import defaultdict
-
 
 def setDefaults(parser):
     """ Set logging config and return args with associated function """
@@ -45,63 +38,6 @@ def createMainParent(verbose=True, version=None):
     return parent
 
 
-class StoreDict(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        kv={}
-        if not isinstance(values, (list,)):
-            values=(values,)
-        for value in values:
-            if value.count('=') != 1:
-                logging.error(
-                    f'Key value data in {value} cannot contain exactly one "=".')
-                raise ValueError
-            n, v = value.split('=')
-            kv[n]=v
-        setattr(namespace, self.dest, kv)
-
-
-def readHomer(matrix, upperOnly=False, sparse=True, diagonal=False, distanceNorm=False, absolute=False):
-    """ Read Homer matrix format and convert to long format """
-
-    # Read Homer as pandas
-    mat = pd.read_csv(matrix, skiprows=1, header=None, sep='\t').drop(0, axis=1)
-    # Split chromosome and start into 2 column DF
-    regions = mat[1].str.split('-', expand=True)
-    startPos = regions[1].astype(int)
-    # Ensure homer is constant binSize matrix
-    binSize = startPos.diff(1).dropna().unique()
-    assert len(binSize) == 1
-    # Drop region column
-    mat = mat.drop(1, axis=1)
-    # Set columns as bin start positions
-    mat.columns = list(startPos)
-    # Set rownames (index) as bin start positions
-    mat['start'] = list(startPos)
-    mat = mat.set_index('start')
-    # Convert to long format
-    mat = mat.stack().rename_axis(['start', 'start2']).rename('score').reset_index()
-    # Remove 0 score rows if sparse set
-    if sparse:
-        mat = mat.loc[mat['score'] != 0]
-    if absolute:
-        mat['score'] = mat['score'].abs()
-    if distanceNorm:
-        mat['seperation'] = abs(mat['start2'] - mat['start'])
-        sumDistance = mat.groupby('seperation').score.agg(['sum', 'count'])
-        sumDistance['expected'] = sumDistance['sum'] / sumDistance['count']
-        mat = pd.merge(mat, sumDistance, on="seperation")
-        mat['score'] = mat['score'] / mat['expected']
-    if upperOnly:
-        mat = mat.loc[mat['start2'] > mat['start']]
-    if diagonal is False:
-        mat = mat.loc[mat['start2'] != mat['start']]
-    # Set chrom from first value since HOMER must be cis-only matrix
-    mat.attrs['chrom'] = regions[0][0]
-    mat.attrs['binSize'] = int(binSize)
-
-    return mat[['start', 'start2', 'score']]
-
-
 def readChromSizes(file):
     """ Read chromosome sizes to dict """
     chromSizes = {}
@@ -113,29 +49,3 @@ def readChromSizes(file):
             chrom, size = line.split()
             chromSizes[str(chrom)] = int(size)
     return chromSizes
-
-
-def getGroup(path):
-    """ Retrieve group name from file path """
-    basename = os.path.basename(path)
-    return basename.split('-')[0]
-
-
-def getBin(pos: int, binSize: int, start: int):
-    matrixBin = (pos - start) // binSize
-    return start + (matrixBin * binSize)
-
-
-def readSUTM(sutm, lower=False, bothDiagonal=False):
-    sutm = pd.read_csv(sutm, names=['start1', 'start2', 'adjIF'], sep=' ')
-    sutm['orientation'] = 1
-    if lower:
-        # Remove the diagonal for the opposite orientation
-        if not bothDiagonal:
-            sltm = sutm.loc[sutm['start1'] != sutm['start2']]
-        else:
-            sltm = sutm.copy()
-        sltm = sltm.rename({'start1': 'start2', 'start2': 'start1'}, axis=1)
-        sltm['orientation'] = -1
-        sutm  = pd.concat([sutm, sltm])
-    return sutm.set_index(['start1', 'start2', 'orientation'])
