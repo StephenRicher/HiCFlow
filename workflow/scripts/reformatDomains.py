704c7015dee15ed294cdc27cb21b053812a28fec
Stephen Richer <sr467@bath.ac.uk>
2022-01-11 16:23:38 +0000

    Simplify

--- workflow/scripts/reformatDomains.py
+++ workflow/scripts/reformatDomains.py
@@ -12,20 +12,14 @@ from utilities import setDefaults, createMainParent
 __version__ = '1.0.0'
 
 
-def reformatDomains(bed: str, scale: int, trimChr: bool, **kwargs):
+def reformatDomains(bed: str):
 
     with fileinput.input(bed) as fh:
         for line in fh:
             # Skip header line
             if line.startswith(('#', 'track')):
                 continue
-            columns = line.split()
-            chrom = columns[0]
-            if chrom.startswith('chr') and trimChr:
-                chrom = chrom[3:]
-            start = str(int(columns[1]) + scale)
-            end = str(int(columns[2]) + scale)
-
+            chrom, start, end = line.split()[:3]
             print(chrom, start, end, '.', '0', '.', start, end, '.', sep='\t')
 
 
@@ -39,13 +33,6 @@ def parseArgs():
     parser.add_argument(
         'bed', nargs='?', default=[],
         help='BED format TAD files for conversion (default: stdin)')
-    parser.add_argument(
-        '--scale', type=int, default=0,
-        help='0-based genomic coordinate of start position '
-             'to rescale (default: %(default)s)')
-    parser.add_argument(
-        '--trimChr', action='store_true',
-        help='Trim chr prefix from chrom name (default: %(default)s)')
     parser.set_defaults(function=reformatDomains)
 
     return setDefaults(parser)
